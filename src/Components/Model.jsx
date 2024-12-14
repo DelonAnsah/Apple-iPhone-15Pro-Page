9a3650b4bbd560b4c0b7a10428a7d334c85c5ef9
Delon Ansah Acquaah <delonansah87@gmail.com>
2024-12-14 16:48:22 +0000

    set up sentry metrics and model for the iphone 15 pro

--- src/Components/Model.jsx
+++ src/Components/Model.jsx
@@ -7,6 +7,7 @@ import * as THREE from 'three'
 import { Canvas } from '@react-three/fiber'
 import { View } from '@react-three/drei'
 import { models, sizes } from '../Constants'
+import { animateWithGsapTimeline } from '../utils/animations'
 
 
 
@@ -37,11 +38,17 @@ const Model = () => {
 
   useEffect(() => {
     if (size === 'large') {
-
+      animateWithGsapTimeline(tl, small, smallRotation,'#view1', '#view2', {
+        transform: 'translateX(-100%)',
+        duration: 2
+      } )
     }
 
     if(size === 'small') {
-      
+      animateWithGsapTimeline(tl, large, largeRotation,'#view2', '#view1', {
+        transform: 'translateX(0)',
+        duration: 2
+      } )
     }
   })
 
